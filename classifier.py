1da7cfd7617a769ffd8b1a9bbdb12c66369c7618
Keara Berlin <kearaberlin@gmail.com>
2023-02-23 19:06:49 -0600

    read text file of filepaths

--- classifier.py
+++ classifier.py
@@ -10,5 +10,11 @@ args = parser.parse_args()
 print(args.filepath)
 print(args.test)
 
+filepaths = []
+with open(args.filepath, 'r') as f:
+    filepaths = [line.strip() for line in f.readlines()]
+
+print(filepaths)
+
 # if __name__ == "__main__":
 #     files_filepath = sys.argv[1]
