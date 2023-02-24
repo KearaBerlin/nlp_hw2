95e75d7ec77de95f113592d2813a4232c5dcacf4
Keara Berlin <kearaberlin@gmail.com>
2023-02-23 20:20:18 -0600

    split train test

--- classifier.py
+++ classifier.py
@@ -1,7 +1,9 @@
 import argparse
 import sys
+import random
 import numpy as np
 import nltk
+from sklearn.model_selection import train_test_split
 
 nltk.download('punkt')
 
@@ -16,27 +18,35 @@ def read_file(filepath):
         filepaths = [line.strip() for line in f.readlines()]
     return filepaths
 
-filepaths = read_file(args.filepath)
-
-# read in files
-# files = np.empty
-files = [[] for i in range(len(filepaths))]
-for (i, filepath) in enumerate(filepaths):
+def extract_sentences(filepath):
     lines = read_file(filepath)
     sentences = [nltk.tokenize.word_tokenize(line) for line in lines]
+    sentences = [s for s in sentences if len(s) > 2]
+    return sentences
+
+filepaths = read_file(args.filepath)
+
+# read in files, which will be a list of lists; each nested list is a list of sentences;
+# each list of sentences is a list of tokens.
+files = [[] for i in range(len(filepaths))]
+for (i, filepath) in enumerate(filepaths):
+    sentences = extract_sentences(filepath)
     files[i] = sentences
-    # files = np.concatenate((files, sentences), axis=1)
-print(len(files))
 
-# each line is a sample
-# throw out samples that are <= 2 words
-
+# train / test split
 if args.testfile:
-    train = [] # TODO read in all data from filepaths
-    test = [] # read from args.testfile
+    train = []
+    for (label, sentences) in enumerate(files):
+        data = [(s, label) for s in sentences]
+        train.extend(data)
+    test = extract_sentences(args.testfile)
 else:
-    train = [] # TODO get 90% of data from each filepaths
+    train = [] # get 90% of data from each filepaths
     test = [] # remaining 10% from each of filepaths
+    for (label, sentences) in enumerate(files):
+        data = [(s, label) for s in sentences]
+        train_test = train_test_split(data, test_size=0.1)
+        train.extend(train_test[0])
+        test.extend(train_test[1])
 
-# if __name__ == "__main__":
-#     files_filepath = sys.argv[1]
+print(f"Train: {len(train)} Test: {len(test)}")
