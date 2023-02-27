94e69777991a9a6bece0cdd4c287987e01e120ce
markge0115 <66687206+markge0115@users.noreply.github.com>
2023-02-26 21:45:35 -0600

    Update classifier.py
    added code to run accuracy

--- classifier.py
+++ classifier.py
@@ -1,4 +1,6 @@
 import argparse
+import math
+import copy
 import sys
 import random
 import numpy as np
@@ -6,6 +8,7 @@ import nltk
 from sklearn.model_selection import train_test_split
 from nltk.util import bigrams
 from nltk.lm.preprocessing import padded_everygram_pipeline
+from nltk.lm import MLE
 # Hi
 nltk.download('punkt')
 
@@ -53,11 +56,47 @@ for filepath in filepaths:
 train_bigram = {}
 test_bigram = {}
 
+lms = {}
+
 for filepath in filepaths:
-    for i in range(len(train[filepath])):
-        # To Do implement following command? train, vocab = padded_everygram_pipeline(2, text)
-        train_bigram[filepath] = list(bigrams(train[filepath][i]))
-        test_bigram[filepath] = list(bigrams(test[filepath][i]))
-    print(len(train_bigram[filepath]))
-    print(len(test_bigram[filepath]))
-    print(train_bigram[filepath][0])
+
+    # for i in range(len(train[filepath])):
+    #     # To Do implement following command? train, vocab = padded_everygram_pipeline(2, text)
+    #     train_bigram[filepath] = list(bigrams(train[filepath][i]))
+    #     test_bigram[filepath] = list(bigrams(test[filepath][i]))
+
+    train_bigram, train_vocab = padded_everygram_pipeline(2, train[filepath])
+
+    lm = MLE(2)
+    lm.fit(train_bigram, train_vocab)
+    lms[filepath] = lm
+    print(lm.vocab)
+    # print(len(train_bigram[filepath]))
+    # print(len(test_bigram[filepath]))
+    # print(train_bigram[filepath][0])
+
+for label in filepaths:
+    test_bigram, test_vocab = padded_everygram_pipeline(2, test[label])
+
+    num_correct = 0
+    for sentence in test_bigram:
+        # new_sentence = copy.deepcopy(sentence)
+        # try:
+        #     next(new_sentence)
+        # except StopIteration:
+        #     print("uh oh")
+        #     continue
+        sentence = [ngram for ngram in sentence]
+
+        best_perplexity = math.inf
+        best_filepath = ""
+        for (filepath, model) in lms.items():
+            perplexity = model.perplexity(sentence)
+            if best_perplexity > perplexity:
+                best_perplexity = perplexity
+                best_filepath = filepath
+        if label == best_filepath:
+            num_correct = num_correct+1
+    accuracy = num_correct/len(test[label])
+    print(accuracy)
+
